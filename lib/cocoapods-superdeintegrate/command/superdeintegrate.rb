2c99cbde80f63ea3529d2be9b028db14d82b1477
Samuel E. Giddins <segiddins@segiddins.me>
2015-06-22 00:00:52 -0700

    Update superdeintegrate.rb

--- lib/cocoapods-superdeintegrate/command/superdeintegrate.rb
+++ lib/cocoapods-superdeintegrate/command/superdeintegrate.rb
@@ -8,13 +8,18 @@ module Pod
       DESC
 
       def run
-        UI.puts("Superdeintegrating directory.".green)
-
-        commands = ['rm -rf ~/Library/Caches/CocoaPods',
-                    'rm -rf ~/Library/Developer/Xcode/DerivedData',
-                    'pod deintegrate || rm -rf Pods']
-
-        commands.each { |command| system(command) }
+        require 'fileutils'
+        require 'pod/command/deintegrate'
+        
+        UI.section "Superdeintegrating directory." do
+          FileUtils.rm_rf config.cache_root
+          FileUtils.rm_rf '~/Library/Developer/Xcode/DerivedData'
+          begin
+            Pod::Command::Deintegrate.invoke
+          rescue
+            FileUtils.rm_rf config.sandbox_root
+          end
+        end
 
         UI.puts("Finished supdeintegrating directory.".green)
       end
